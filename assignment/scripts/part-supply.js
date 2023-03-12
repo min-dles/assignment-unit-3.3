cbe95a9213618e84f2901a7ad138a945e44c0e5d
mandi andrade <mandi.andrade20@gmail.com>
2023-03-12 00:43:03 -0600

    edited answer for 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -35,7 +35,9 @@ console.log('Supply Changes has been updated and is now:', supplyChanges);
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 for(let i = 0; i < supplyChanges.length; i++){
+    if(supplyChanges[i] > 0){
     console.log('Added', supplyChanges[i], 'parts.');
+    }
     if(supplyChanges[i] === 0){
         console.log('No Change.');
     }
